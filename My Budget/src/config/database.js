c25eedae7cb64111e1e86f9f124f5463dd7e9ae8
Mateus Getulio Vieira <mateus.getulio@gmail.com>
2019-07-29 12:02:59 -0300

    Changes to the sample files
    Changes to the sample files

--- My Budget/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-module.exports = mongoose.connect("mongodb://user:pass@mongo.com:57574/db");
-
-mongoose.Error.messages.general.required =
-  "The attribute '{PATH}' must be filled.";
-mongoose.Error.messages.Number.min = "The '{VALUE}' is smaller than '{MIN}'.";
-mongoose.Error.messages.Number.max = "The '{VALUE}' is higher than '{MAX}'.";
-mongoose.Error.messages.String.enum = "'{VALUE}' isn't valid for the '{PATH}'.";
--- PetShow/Controller/admin_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdminController < ApplicationController
-  layout "admin/default"
-  before_action :require_user
- 
-  def index
-    
-  end
-
-end
